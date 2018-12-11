0ccbeba76b1ef7c65aba54fe453bbb33944d9946
Thijs Miedema <t.miedema@live.nl>
2018-12-11 16:08:43 +0100

    Test for override behaviour. Replicates MarTeXPHP

--- tests/modules.php
+++ tests/modules.php
@@ -40,3 +40,33 @@ class testenv1 extends MarTeX\Environment
 
 runmtest("M.1", array("testmodule1", "testmodule2", "testmodule3", "testmodule4"), "\\yays2\\begin{t}\\yays1\\yayp\\end{t}\\yayp", "hello, world!");
 
+class testmodule5 extends MarTeX\Module
+{
+    public function globals() {return array("yay");}
+    public function environments(){return array("t" => "testenv2");}
+    public function yay($env, $args){
+        return array(array(MarTeX\TypeString, "Nothing good!"));
+    }
+}
+
+class testenv2 extends MarTeX\Environment
+{
+    public function end($d) { return array(array(MarTeX\TypeString, "Can't override environments,"));}
+}
+
+class testmodule6 extends MarTeX\Module
+{
+    public function globals() {return array("yay");}
+    public function environments(){return array("t" => "testenv3");}
+    public function yay($env, $args){
+        return array(array(MarTeX\TypeString, "but you can override commands!"));
+    }
+}   
+
+class testenv3 extends MarTeX\Environment
+{
+}
+
+/// Command and Environment overrides
+
+runmtest("M.2", array("testmodule5", "testmodule6"), "\\begin{t}\\end{t} \\yay", "Can't override environments, but you can override commands!");
